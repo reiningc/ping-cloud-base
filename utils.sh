f1f9d825abfe389aae1981ac3d01c73157d17f12
Jack Miner <jackminer@pingidentity.com>
2022-12-30 14:01:35 -0700

    Attempt to deploy with cmctl

--- utils.sh
+++ utils.sh
@@ -665,7 +665,8 @@ apply_crds() {
   # must be available before we make use cert-manager
   kubectl apply -f "${base_dir}/k8s-configs/cluster-tools/base/cert-manager/base/cert-manager.yaml"
   # Wait until the webhook deployment is fully available
-  wait_for_rollout "deployment/cert-manager-webhook" "cert-manager" "20"
+  #wait_for_rollout "deployment/cert-manager-webhook" "cert-manager" "20"
+  cmctl check api --wait=2m
 
   # argo-events CRDs
   argo_crd_yaml="${base_dir}/k8s-configs/cluster-tools/base/notification/argo-events/argo-events-crd.yaml"
