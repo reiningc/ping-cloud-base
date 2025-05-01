df20944ba647473b6126089500c7b6a090a5eddd
Colin Reininger <colinreininger@pingidentity.com>
2025-05-01 15:41:04 -0600

    verbose git clone command

--- code-gen/generate-cluster-state.sh
+++ code-gen/generate-cluster-state.sh
@@ -1512,7 +1512,7 @@ for ENV_OR_BRANCH in ${SUPPORTED_ENVIRONMENT_TYPES}; do
     log "Cloning ${app_repo}@${app_repo_branch} to '${PROFILE_REPO_MIRROR_DIR}' ${MICROSERVICE_APP_REPO_URL}/p1as-apps/${app_repo}"
     log "$(git config --get user.name)"
     log "$(git config --get user.email)"
-    git clone -c advice.detachedHead=false --depth 1 --branch "${app_repo_branch}" "${MICROSERVICE_APP_REPO_URL}/p1as-apps/${app_repo}" "${PROFILE_REPO_MIRROR_DIR}/${app_repo}"
+    git clone -vvv -c advice.detachedHead=false --depth 1 --branch "${app_repo_branch}" "${MICROSERVICE_APP_REPO_URL}/p1as-apps/${app_repo}" "${PROFILE_REPO_MIRROR_DIR}/${app_repo}"
 
     if test $? -ne 0; then
       log "Unable to clone ${app_repo}@${app_repo_branch} from ${MICROSERVICE_APP_REPO_URL}/p1as-apps"
